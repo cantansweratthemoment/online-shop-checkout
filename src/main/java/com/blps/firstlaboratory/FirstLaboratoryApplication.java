b0110e8eac7aff504156104125c2d2904e50b078
HusseinHalifaev <a.alibekovich@yandex.ru>
2022-02-19 22:24:35 +0300

    init

---
+++ src/main/java/com/blps/firstlaboratory/FirstLaboratoryApplication.java
@@ -0,0 +1,13 @@
+package com.blps.firstlaboratory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstLaboratoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstLaboratoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blps/firstlaboratory/FirstLaboratoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blps.firstlaboratory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstLaboratoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
