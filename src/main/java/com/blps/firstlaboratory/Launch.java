9e477a7ab7c6a26eeb8bf5ff62f73fa1e244ea94
HusseinHalifaev <a.alibekovich@yandex.ru>
2022-02-19 23:06:16 +0300

    + database configuration

--- src/main/java/com/blps/firstlaboratory/Launch.java
+++ src/main/java/com/blps/firstlaboratory/Launch.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FirstLaboratoryApplication {
+public class Launch {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirstLaboratoryApplication.class, args);
+		SpringApplication.run(Launch.class, args);
 	}
 
 }
