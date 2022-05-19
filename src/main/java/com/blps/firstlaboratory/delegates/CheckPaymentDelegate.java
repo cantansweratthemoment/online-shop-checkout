ddb778d83c002eb2b5f0b19c71632534388835b4
Olyamba <ilinskayaov@yandex.ru>
2022-05-20 00:18:42 +0300

    Update CheckPaymentDelegate.java

--- src/main/java/com/blps/firstlaboratory/delegates/CheckPaymentDelegate.java
+++ src/main/java/com/blps/firstlaboratory/delegates/CheckPaymentDelegate.java
@@ -2,6 +2,7 @@ package com.blps.firstlaboratory.delegates;
 
 import com.blps.firstlaboratory.services.CheckPaymentService;
 import lombok.RequiredArgsConstructor;
+import org.camunda.bpm.engine.IdentityService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,7 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class CheckPaymentDelegate implements JavaDelegate {
     private final CheckPaymentService checkPaymentService;
+    private IdentityService identityService;
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
@@ -23,10 +25,17 @@ public class CheckPaymentDelegate implements JavaDelegate {
         String[] products = productsS.split(" ");
         String country = (String) delegateExecution.getVariable("country");
         String region = (String) delegateExecution.getVariable("region");
-        ResponseEntity<String> result = checkPaymentService.checkPayment(products, login, country, region);
-        if(Objects.requireNonNull(result.getBody()).equals("Payment successful!")){
+        boolean admin = identityService.getCurrentAuthentication().getGroupIds().contains("camunda-admin");
+        ResponseEntity<String> result = null;
+        System.out.println("VALUEV = " + admin);
+        if (admin) {
+            result = checkPaymentService.checkAdminPayment(products, login, country, region);
+        } else {
+            result = checkPaymentService.checkPayment(products, login, country, region);
+        }
+        if (Objects.requireNonNull(result.getBody()).equals("Payment successful!")) {
             delegateExecution.setVariable("is_payment", true);
-        }else {
+        } else {
             delegateExecution.setVariable("is_payment", false);
         }
     }
